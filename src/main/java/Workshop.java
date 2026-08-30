30e09c78de1d0a093a09e18a1310d4d830474f37
Milevibes <ycespedesp@udistrital.edu.co>
2026-08-30 14:23:23 -0500

    Resueltos ejercicios mayor y menor

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -112,14 +112,28 @@ public class Workshop {
     public int encontrarElementoMayor(int[] arreglo) {
         // TODO: Implementar el método para encontrar el elemento mayor en un arreglo.
         // Ejemplo: Si arreglo = [1, 2, 3, 4, 5], el resultado debería ser 5.
-        
+        if (arreglo.length == 0) return 0;
+	int mayor = arreglo[0];
+	for (int num : arreglo) {
+		if (num > mayor) {
+			mayor = num;
+		}
+	}
+	return mayor;
     }
 
     // Método que encuentra el elemento menor en un arreglo
     public int encontrarElementoMenor(int[] arreglo) {
         // TODO: Implementar el método para encontrar el elemento menor en un arreglo.
         // Ejemplo: Si arreglo = [1, 2, 3, 4, 5], el resultado debería ser 1.
-        return 0;
+        if (arreglo.length == 0) return 0;
+	int menor = arreglo[0];
+	for (int num : arreglo) {
+		if (num < menor) {
+			menor = num;
+		}
+	}
+	return menor;
     }
 
     // Método que busca un elemento en un arreglo
