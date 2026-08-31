ffd6c5289ec75c68e5b347a641b97bd1a041cb21
Milevibes <ycespedesp@udistrital.edu.co>
2026-08-31 06:08:12 -0500

    Resuelto ejercicio buscarElemento

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -140,7 +140,12 @@ public class Workshop {
     public boolean buscarElemento(int[] arreglo, int elemento) {
         // TODO: Implementar el método para buscar un elemento en un arreglo.
         // Ejemplo: Si arreglo = [1, 2, 3, 4, 5] y elemento = 3, el resultado debería ser true.
-        return false;
+	for (int num : arreglo) {
+		if (num == elemento) {
+			return true;
+		}
+	}       
+	return false;
     }
 
     // Método que invierte un arreglo
