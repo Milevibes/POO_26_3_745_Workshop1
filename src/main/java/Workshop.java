398152060c04b0ff528b06b19a521f19e27e2459
Milevibes <ycespedesp@udistrital.edu.co>
2026-08-31 06:18:48 -0500

    Resuelto ejercicio ordenarArreglo

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -152,19 +152,21 @@ public class Workshop {
     public int[] invertirArreglo(int[] arreglo) {
         // TODO: Implementar el método para invertir un arreglo.
         // Ejemplo: Si arreglo = [1, 2, 3, 4, 5], el resultado debería ser [5, 4, 3, 2, 1].
-        return new int[0];
-    }
-
-    // Método que ordena un arreglo en orden ascendente
-    public int[] ordenarArreglo(int[] arreglo) {
-        // TODO: Implementar el método para ordenar un arreglo en orden ascendente.
-        // Ejemplo: Si arreglo = [5, 4, 3, 2, 1], el resultado debería ser [1, 2, 3, 4, 5].
         int n = arreglo.length;
 	int[] invertido = new int[n];
 	for (int i = 0; i < n; i++) {
 		invertido[i] = arreglo[n - 1 - i];
 	}
 	return invertido;
+    }
+
+    // Método que ordena un arreglo en orden ascendente
+    public int[] ordenarArreglo(int[] arreglo) {
+        // TODO: Implementar el método para ordenar un arreglo en orden ascendente.
+        // Ejemplo: Si arreglo = [5, 4, 3, 2, 1], el resultado debería ser [1, 2, 3, 4, 5].
+        int[] ordenado = arreglo.clone();
+	java.util.Arrays.sort(ordenado);
+	return ordenado;
     }
 
     // Método que elimina los duplicados de un arreglo
